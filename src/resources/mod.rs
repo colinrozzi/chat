e97205ac231284902b9d29662d9e897832a2f695
colin <colinrozzi@gmail.com>
2025-04-08 10:14:45 -0400

    feat: add static resource embedding module

---
+++ src/resources/mod.rs
@@ -0,0 +1,19 @@
+// Embed static files directly into the binary
+// HTML files
+pub const INDEX_HTML: &str = include_str!("../../assets/index.html");
+
+// CSS files
+pub const STYLES_CSS: &str = include_str!("../../assets/styles.css");
+
+// JavaScript files
+pub const CHAT_JS: &str = include_str!("../../assets/chat.js");
+
+// Function to get a resource by path
+pub fn get_resource(path: &str) -> Option<(&'static str, &'static str)> {
+    match path {
+        "/" | "/index.html" => Some((INDEX_HTML, "text/html")),
+        "/styles.css" => Some((STYLES_CSS, "text/css")),
+        "/chat.js" => Some((CHAT_JS, "application/javascript")),
+        _ => None,
+    }
+}
