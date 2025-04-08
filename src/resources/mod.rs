21589ca82e890bcda2bbc02311ce33576d0a2e92
colin <colinrozzi@gmail.com>
2025-04-08 10:30:33 -0400

    feat(resources): add logging for resource requests

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,3 +1,4 @@
+use crate::bindings::ntwk::theater::runtime::log;
 // Embed static files directly into the binary
 // HTML files
 pub const INDEX_HTML: &str = include_str!("../../assets/index.html");
@@ -10,6 +11,7 @@ pub const CHAT_JS: &str = include_str!("../../assets/chat.js");
 
 // Function to get a resource by path
 pub fn get_resource(path: &str) -> Option<(&'static str, &'static str)> {
+    log(format!("Request for resource: {}", path).as_str());
     match path {
         "/" | "/index.html" => Some((INDEX_HTML, "text/html")),
         "/styles.css" => Some((STYLES_CSS, "text/css")),
