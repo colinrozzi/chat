91f633e5a003ac8cbb5d503650c6e4bc5747603f
colin <colinrozzi@gmail.com>
2025-04-11 11:52:34 -0400

    chore: add nix package and update devshell

--- flake.nix
+++ flake.nix
@@ -11,11 +11,29 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "chat-actor";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          # Optional: remove if not needed
+          buildInputs = [ ];
+
+          # If you want to test the binary after building
+          doCheck = false;
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustup
+            cargo
           ];
-          
+
           shellHook = ''
             echo "Chat Actor Development Environment"
           '';
